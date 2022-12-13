797243aef3e0e9ac4531624fd941d4eef0abf761
Ambrose <ambroseheng@gmail.com>
2022-12-13 10:44:06 +0800

    Commented out user

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/users");
+// const User = require("../models/users");
 
-router.get("/seed", async (req, res) => {
-  const users = [
-    {
-      username: "grapefruit",
-      email: "pink@fruits.com",
-      password: "fruitbasket",
-    },
-  ];
-  try {
-    await User.deleteMany({}); //* delete all users
-    const newUsers = await User.create(users);
-    res.json(newUsers);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+// router.get("/seed", async (req, res) => {
+//   const users = [
+//     {
+//       username: "grapefruit",
+//       email: "pink@fruits.com",
+//       password: "fruitbasket",
+//     },
+//   ];
+//   try {
+//     await User.deleteMany({}); //* delete all users
+//     const newUsers = await User.create(users);
+//     res.json(newUsers);
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
 
-router.get("/", async (req, res) => {
-  try {
-    const users = await User.find({}).exec();
-    console.log(users);
-    res.json(users);
-  } catch (error) {
-    res.status(500).json(error);
-    console.log(error);
-  }
-});
+// router.get("/", async (req, res) => {
+//   try {
+//     const users = await User.find({}).exec();
+//     console.log(users);
+//     res.json(users);
+//   } catch (error) {
+//     res.status(500).json(error);
+//     console.log(error);
+//   }
+// });
 
 module.exports = router;
