c185890c1a729d7d67abacb663d0c4db0e623731
Ambrose <ambroseheng@gmail.com>
2022-12-14 19:57:29 +0800

    Display page & route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Holidays from "./pages/Holidays";
+import ClassroomDisplay from "./pages/ClassroomDisplay";
 import Error from "./pages/Error";
 
 function App() {
@@ -15,29 +16,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/holidays" element={<Holidays />} />
+          <Route path="/classroomdisplay" element={<ClassroomDisplay />} />
+          <Route path="/classroomdisplay:id" element={<ClassroomDisplay />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </div>
   );
 }
