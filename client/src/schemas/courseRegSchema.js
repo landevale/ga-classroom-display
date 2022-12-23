3a4aa3953260bbfc26bb761294ea42c0a2ef7e55
Ambrose <ambroseheng@gmail.com>
2022-12-23 10:31:46 +0800

    End date validation

--- client/src/schemas/courseRegSchema.js
+++ client/src/schemas/courseRegSchema.js
@@ -6,7 +6,9 @@ export const courseRegSchema = Yup.object().shape({
     .oneOf(["FullTime", "PartTime"])
     .required("Course type is required"),
   startDate: Yup.date().required("Start date is required"),
-  endDate: Yup.date().required("End Date is required"),
+  endDate: Yup.date()
+    .min(Yup.ref("startDate"), "End date can't be before Start date")
+    .required("End Date is required"),
   daysOnCampus: Yup.object().shape({
     monday: Yup.boolean(),
     tuesday: Yup.boolean(),
@@ -19,40 +21,3 @@ export const courseRegSchema = Yup.object().shape({
   endTime: Yup.string(),
   classRoom: Yup.number().integer().positive().min(1).max(6),
 });
-
-//   {
-//     courseCode: {
-//       type: String,
-//       // uppercase: true,
-//       unique: true,
-//       required: [true, "can't be blank"],
-//       index: true,
-//       trim: true,
-//     },
-//     courseSchedule: { type: String }, //PartTime or FullTime // trim
-//     startDate: {
-//       type: String,
-//       required: true,
-//       index: true,
-//     }, // dates
-//     endDate: {
-//       type: String,
-//       required: true,
-//       index: true,
-//     }, // dates
-//     daysOnCampus: {
-//       days: {
-//         monday: { type: Boolean, default: false },
-//         tuesday: { type: Boolean, default: false },
-//         wednesday: { type: Boolean, default: false },
-//         thursday: { type: Boolean, default: false },
-//         friday: { type: Boolean, default: false },
-//       },
-//     }, // remove the nest
-//     startTime: { type: String }, //need start/end-time(?) Do we need to display "hours" in daily calendar?
-//     endTime: { type: String },
-//     classRoom: { type: Number },
-//     weeks: { type: Number },
-//     studentSuccess: { type: String },
-//     altSaturdays: { type: String, required: true }, //none, odd, even, ALL
-//   },
